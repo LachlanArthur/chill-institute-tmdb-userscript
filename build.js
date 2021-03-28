5c16942e0cceea63538cf63fde7b214adf02ea34
Lachlan Arthur <git@me.lach.la>
2021-03-28 23:08:19 +1000

    Insert UserScript banner

--- build.js
+++ build.js
@@ -7,6 +7,16 @@ require( 'esbuild' ).build( {
 	format: 'iife',
 	sourcemap: false,
 	treeShaking: true,
+	banner: [
+		'// ==UserScript==',
+		'// @name         TMDB Movie Search for Chill Institute',
+		'// @version      1.0',
+		'// @author       Cyykratahk',
+		'// @match        https://chill.institute',
+		'// @grant        none',
+		'// ==/UserScript==',
+		'',
+	].join('\n'),
 	inject: [
 		'src/jsx-shim.ts',
 	],
